af07f751d806025bd8c36698959968e361c2638b
zhiming.sun <zhiming.sun@qq.com>
2025-01-08 22:17:48 +0800

    up

---
+++ Scripts/win/git_configure.sh
@@ -0,0 +1,173 @@
+#!/bin/bash
+
+set -e
+
+windows_terminal_home="D:\WindowsTerminal"
+windows_terminal_version="1.20.11781.0"
+
+
+# link
+mklink_opt_windows="D:\opt"
+mklink_opt_linux="$EXEPATH\..\opt" # /opt
+
+mklink_local_windows="D:\local"
+mklink_local_linux="$EXEPATH\..\usr\local" # /usr/local
+
+
+# linux 目录链接, 用于在 bash 环境中模拟 lunux 目录结构
+linux_symbolic() {
+  echo -e "\033[32mconfigure linux symbolic link.\033[0m"
+
+  # add mklink.bat
+  cat > mklink.bat << EOF
+@echo off
+::echo param[0] = %0
+::echo param[1] = %1
+::echo param[2] = %2
+mklink /J %1 %2
+EOF
+
+  if [[ ! -d "$mklink_opt_linux" ]]; then
+    if [[ ! -d "$mklink_opt_windows" ]]; then
+      mkdir -p "$mklink_opt_windows"
+    fi
+    ./mklink.bat "$mklink_opt_linux" "$mklink_opt_windows" &>/dev/null
+  fi
+
+  if [[ ! -d "$mklink_local_linux" ]]; then
+    if [[ ! -d "$mklink_local_windows" ]]; then
+      mkdir -p "$mklink_local_windows"
+    fi
+    ./mklink.bat "$mklink_local_linux" "$mklink_opt_windows" &>/dev/null
+  fi
+
+  rm -rf mklink.bat
+}
+
+
+git_configure_vimrc() {
+  # /etc/vimrc
+  echo -e "\033[32mconfigure /etc/vimrc\033[0m"
+  if ! grep -q $USERNAME "/etc/vimrc" ; then
+    cat >> /etc/vimrc << EOF
+" by $USERNAME
+set noeb                     " 去除错误提示音
+set number                   " 显示行号
+set autoread                 " 自动加载文件改动
+set expandtab                " 替换 Tab
+set cursorline               " 突出显示当前行
+set ignorecase               " 搜索忽略大小写
+set noswapfile               " 禁用 swp 文件
+
+set tabstop=2                " Tab键的宽度
+set cmdheight=2              " 命令行高度
+
+" set termguicolors            " 开启真彩色
+" colorscheme habamax          " habamax pablo slate wildcharm
+EOF
+  fi
+}
+
+
+git_configure_inputrc() {
+  # git-bash 删除键闪屏
+  echo -e "\033[32mconfigure /etc/inputrc\033[0m"
+
+  sed -i -s 's/set bell-style visible/set bell-style none/g' /etc/inputrc
+
+  # 历史记录前缀搜索
+  if ! grep -q $USERNAME "/etc/inputrc" ; then
+    cat >> /etc/inputrc << EOF
+# by $USERNAME
+"\e[A": history-search-backward
+"\e[B": history-search-forward
+EOF
+  fi
+}
+
+
+git_configure_profile() {
+  # /etc/profile.d/aliases.sh
+  # 移除 /etc/profile.d/aliases.sh 中 "alias ls='ls -F'" 部分，移除文件夹后面的 "/"
+  echo -e "\033[32mconfigure /etc/profile.d/aliases.sh\033[0m"
+  sed -i -s "s/^alias ls='ls -F /alias ls='ls /" /etc/profile.d/aliases.sh
+
+  # /etc/profile.d/$USERPROFILE.sh
+  echo -e "\033[32mconfigure /etc/profile.d/$USERNAME.sh\033[0m"
+  if [[ ! -f "/etc/profile.d/$USERNAME.sh" ]]; then
+    cat > /etc/profile.d/$USERNAME.sh << EOF
+#!/usr/bin/env bash
+
+# 将每个 session 的历史记录行追加到历史文件中
+PROMPT_COMMAND='history -a'
+
+# alias
+alias l='ls -lhv'
+alias la='ls -alhv'
+alias open='start "" '
+
+EOF
+  fi
+
+  # git-prompt.sh
+  echo -e "\033[32mconfigure /etc/profile.d/git-prompt.sh\033[0m"
+  curl -s -L https://raw.githubusercontent.com/charlesbases/applications/master/Git/git-prompt.sh --output /etc/profile.d/git-prompt.sh
+}
+
+
+git_configure_gitconfig() {
+  # ~/.gitconfig (全局配置)
+  echo -e "\033[32mconfigure gitconfig in $USERPROFILE\.gitconfig\033[0m"
+  if [[ ! -f "$USERPROFILE\.gitconfig" ]]; then
+    curl -s -L https://raw.githubusercontent.com/charlesbases/applications/master/Git/gitconfig --output "$USERPROFILE\.gitconfig"
+  fi
+}
+
+
+git_configure() {
+  git_configure_vimrc
+  git_configure_inputrc
+  git_configure_profile
+  git_configure_gitconfig
+}
+
+
+install_windows_terminal() {
+  if [[ -d "$windows_terminal_home" ]]; then return; fi
+
+  echo && read -n 1 -p "Do you need to install Windows Terminal? (y/n)" install && echo
+  if [[ ! "$install" =~ ^[yY]$ ]]; then exit; fi
+
+  echo -e "\033[32mWindowsTerminal downloading...\033[0m"
+  curl -s -L https://github.com/microsoft/terminal/releases/download/v${windows_terminal_version}/Microsoft.WindowsTerminal_${windows_terminal_version}_x64.zip --output windows_terminal_${windows_terminal_version}.zip
+
+  echo -e "\033[32mWindowsTerminal installing...\033[0m"
+  unzip -q windows_terminal_${windows_terminal_version}.zip -d windows_terminal
+  cp -r "windows_terminal/terminal-$windows_terminal_version" "$windows_terminal_home"
+  rm -rf windows_terminal*
+
+  # settings.json
+  echo -e "\033[32mWindowsTerminal configuring...\033[0m"
+  local appdata="$USERPROFILE\AppData\Local\Microsoft\Windows Terminal"
+  if [[ ! -d "$appdata" ]]; then
+    mkdir -p "$appdata"
+  fi
+  curl -s -L https://raw.githubusercontent.com/charlesbases/applications/master/WindowsTerminal/settings.json --output "$appdata\settings.json"
+  echo -e "\033[35mWindowsTerminal 1.20.11781.0 install complete.\033[0m"
+}
+
+
+# main
+read -n 1 -p "Please make sure the proxy is enabled. Continue? (y/n)" input && echo
+if [[ ! "$input" =~ ^[yY]$ ]]; then exit; fi
+
+linux_symbolic
+git_configure
+
+echo -e "\033[35mGit for Windows configure complete.\033[0m"
+
+
+# install others
+install_windows_terminal
+
+echo && read -n 1 -p "Please enter any key to exit." input
---
+++ Scripts/win/go_install.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+
+GO_VERSION="1.22.10"
+
+GOHOME="D:\local\go"
+GOHOME_LINUX="/usr/local/go" # to linux  `mklink /J D:\local\git\bin\..\opt `
+
+GOPATH="D:\opt\go"
+GOPATH_LINUX="/opt/go" # to linux
+
+GOSUMDB="off"
+GOPROXY="https://goproxy.io,direct"
+GO111MODULE="on"
+
+# goland
+IDE_HOME="D:\JetBrains\goland"
+IDE_VERSION="2024.1.6"
+IDE_VERSION_SHORT="2024.1"
+
+#
+environment_user="HKCU:\Environment"
+environment_system="HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+
+
+windows_alias_install() {
+  # start
+  cat > start.bat << EOF
+@echo off
+::echo param[0] = %0
+::echo param[1] = %1
+start /wait "" %1
+EOF
+}
+
+
+windows_alias_uninstall() {
+  rm -rf "start.bat"
+}
+
+
+go_install() {
+  if [[ "$GOHOME" ]]; then return; fi
+
+  echo -e "\033[32mgo installing...\033[0m"
+
+  # install
+  curl -s -L "https://dl.google.com/go/go${GO_VERSION}.windows-amd64.msi" --output "go${GO_VERSION}.msi"
+  ./start.bat "go${GO_VERSION}.msi"
+  rm -rf "go${GO_VERSION}.msi"
+}
+
+
+# 添加 windows 系统变量
+# $1: key
+# $2: value
+configure_windows_environment() {
+  # 判断系统变量是否非空
+  if [[ "$(powershell -Command "Get-ItemProperty '$environment_system'" | awk -F' ' 'NF>0 {print $1}' | grep -w $1)" ]]; then
+    return
+  fi
+
+  echo -e "\033[32m  add windows environment $1=$2\033[0m"
+  powershell -Command "Set-ItemProperty -Path '$environment_system' -Name '$1' -Value '$2'"
+}
+
+
+configure_windows() {
+  # 删除用户变量
+  powershell -Command "Remove-ItemProperty -Path '$environment_user' -Name 'PATH'" &>/dev/null
+  powershell -Command "Remove-ItemProperty -Path '$environment_user' -Name 'GOPATH'" &>/dev/null
+
+  configure_windows_environment "GOPATH" "$GOPATH"
+  configure_windows_environment "GOSUMDB" "$GOSUMDB"
+  configure_windows_environment "GOPROXY" "$GOPROXY"
+  configure_windows_environment "GO111MODULE" "$GO111MODULE"
+
+  # add to PATH
+  if [[ -z "$(echo $PATH | awk -v RS=: 'NF>0 {print}' | grep -w "$GOPATH_LINUX/bin")" ]]; then
+    local current_path="$(powershell -Command "(Get-ItemProperty '$environment_system' -Name 'PATH').PATH")"
+    powershell -Command "Set-ItemProperty -Path '$environment_system' -Name 'PATH' -Value '$current_path;$GOPATH\bin'"
+  fi
+}
+
+
+configure_gitforwindows_environment() {
+  if [[ -z "$(grep -w "^# golang" /etc/profile.d/$USERNAME.sh)" ]]; then
+    echo -e "\033[32m  add git environment GOHOME=$GOHOME_LINUX\033[0m"
+    echo -e "\033[32m  add git environment GOPATH=$GOPATH_LINUX\033[0m"
+    echo -e "\033[32m  add git environment GOSUMDB=off\033[0m"
+    echo -e "\033[32m  add git environment GOPROXY=https://goproxy.io,direct\033[0m"
+    echo -e "\033[32m  add git environment GO111MODULE=on\033[0m"
+
+    cat >> /etc/profile.d/$USERNAME.sh << EOF
+
+# golang
+export GOHOME="$GOHOME_LINUX"
+export GOPATH="$GOPATH_LINUX"
+export GOSUMDB="off"
+export GOPROXY="https://goproxy.io,direct"
+export GO111MODULE="on"
+
+alias cs="cd $GOPATH_LINUX/src"
+EOF
+
+    source /etc/profile.d/$USERNAME.sh
+    mkdir -p $GOPATH/{bin,pkg,src}
+  fi
+}
+
+
+configure_gitforwindows() {
+  configure_gitforwindows_environment
+}
+
+
+go_configure() {
+  echo -e "\033[32mgo configuring...\033[0m"
+
+  configure_windows
+  configure_gitforwindows
+}
+
+
+ide_install() {
+  if [[ -d "$IDE_HOME" ]]; then return; fi
+
+  echo && read -n 1 -p "Do you need to install goland-$IDE_VERSION? (y/n)" install && echo
+  if [[ ! "$install" =~ ^[yY]$ ]]; then exit; fi
+
+  echo -e "\033[32mgoland downloading...\033[0m"
+  curl -s -L "https://download.jetbrains.com/go/goland-$IDE_VERSION.exe" --output goland-$IDE_VERSION.exe
+
+  echo -e "\033[32mgoland installing...\033[0m"
+  ./start.bat "goland-$IDE_VERSION.exe"
+
+  echo -e "\033[32mgoland configuring...\033[0m"
+  local appdata="$USERPROFILE\AppData\Roaming\JetBrains\GoLand$IDE_VERSION_SHORT"
+  if [ -d "$appdata" ]; then rm -rf $appdata; fi
+  mkdir -p $appdata &>/dev/null
+
+  curl -s -L https://raw.githubusercontent.com/charlesbases/applications/master/JetBrains/goland/win/$IDE_VERSION_SHORT/roaming.zip --output roaming.zip
+  unzip -o -q roaming.zip -d $appdata
+
+  curl -s -L https://raw.githubusercontent.com/charlesbases/applications/master/JetBrains/goland/win/$IDE_VERSION_SHORT/setting.zip --output setting.zip
+  unzip -o -q setting.zip -d $appdata
+
+  rm -rf goland-$IDE_VERSION.exe roaming.zip setting.zip
+
+  # 删除安装目录下 llmInstaller
+  rm -rf "$IDE_HOME\plugins\llmInstaller"
+}
+
+
+# main
+read -n 1 -p "Please make sure the proxy is enabled. Continue? (y/n)" input && echo
+if [[ ! "$input" =~ ^[yY]$ ]]; then exit; fi
+
+windows_alias_install
+
+go_install
+go_configure
+echo -e "\033[35mgo$GO_VERSION install complete.\033[0m"
+
+# install ide
+ide_install
+
+windows_alias_uninstall
+
+echo && read -n 1 -p "Please enter any key to exit." input
